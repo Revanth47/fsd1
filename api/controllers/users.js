6449f7a0b07430155dabb4b7b4e005d6a639f6cb
Revanth M <7revanth47@gmail.com>
2019-04-19 13:39:46 +0530

    Add suggestions and tweets

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,14 +1,17 @@
 const users = require('../data/users.json')
 const media = require('../data/media.json')
+const tweets = require('../data/tweets.json')
 
 function userDetails(req, res) {
     const userId = Number(req.swagger.params.userId.value)
     const targetUser = users.find(u => u.id === userId)
+    
     if(targetUser) {
         return res.json({
             data: targetUser
         })
     }
+
     return res.status(400).json({
         message: "User with the given ID does not exist"
     })
@@ -16,12 +19,56 @@ function userDetails(req, res) {
 
 function userMedia(req, res) {
     const userId = Number(req.swagger.params.userId.value)
+    const userData = users.find(u => u.id === userId)
+    if(!userData) {
+        return res.status(400).json({
+            message: "User with the given ID does not exist"
+        })
+    }
+
     const results = media.filter(d => d.user_id === userId)
 
-    return res.json({data: results})
+    return res.json({ data: results })
+}
+
+function userFollowersSuggestions(req, res) {
+    const userId = Number(req.swagger.params.userId.value)
+    const userData = users.find(u => u.id === userId)
+    if(!userData) {
+        return res.status(400).json({
+            message: "User with the given ID does not exist"
+        })
+    }
+
+    const currentlyFollowing = userData.following || []
+    const suggestions = users.filter(u => u.id !== userId && !currentlyFollowing.includes(u.id))
+
+    return res.json({ data: suggestions })
+}
+
+/**
+ * Tweets made by the people the user is following
+ */
+function userFollowingTweets(req, res) {
+    const userId = Number(req.swagger.params.userId.value)
+    const userData = users.find(u => u.id === userId)
+    if(!userData) {
+        return res.status(400).json({
+            message: "User with the given ID does not exist"
+        })
+    }
+
+    const currentlyFollowing = userData.following || []
+    const data = tweets.filter(t => currentlyFollowing.includes(t.userId)).map(t => ({
+        ...t,
+        user: users.find(u => u.id === t.userId) || {}
+    }))
+    return res.json({ data })
 }
 
 module.exports = {
     userDetails,
-    userMedia
+    userMedia,
+    userFollowersSuggestions,
+    userFollowingTweets
 }
